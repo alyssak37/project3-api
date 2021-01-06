44228fadb4741ecee98dfe3a28a08cced801ed9c
Alyssa Karakula <alyssakarakula@Alyssas-MacBook-Air.local>
2021-01-05 22:39:06 -0500

    adds signup

---
+++ routes/api/users.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const { get } = require('http');
+
+const router = require('express').Router();
+const usersCtrl = require('../../controllers/users');
+
+router.post('/signup', usersCtrl.signup);
+
+module.exports = router;
