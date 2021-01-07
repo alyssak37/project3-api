71a5dac2f0744c71fe3834dbff2843ab2da30241
Alyssa Karakula <alyssakarakula@Alyssas-MacBook-Air.local>
2021-01-06 21:46:18 -0500

    installs and configs cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
 require('./config/database');
 
@@ -8,8 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(logger('dev'));
 
-//app.use(cors());
-//app.use('/api/users', require('./routes/api/users'))
+app.use(cors());
+app.use('/api/users', require('./routes/api/users'))
 
 const port = process.env.PORT || 3001
 
